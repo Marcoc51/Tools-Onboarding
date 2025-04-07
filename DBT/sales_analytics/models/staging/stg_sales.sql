8445515fce61802406ea0c5aad1db3dd414b4fa3
Marc Sanad <marcoatef222@gmail.com>
2025-04-07 12:54:44 +0200

    Onboard on dbt with BigQuery dwh

---
+++ DBT/sales_analytics/models/staging/stg_sales.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+SELECT
+    order_id,
+    product_name,
+    amount,
+    country,
+    order_date,
+    {{ dynamic_partition('order_date', 'MONTH') }}  -- Dynamic partitioning
+FROM 
+    `sales-analytics-456110.sales_dataset.raw_sales`
